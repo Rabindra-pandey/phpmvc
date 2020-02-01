502288e8946714a95661b03edaaaa3e83147ed35
Rabindra Nath Pandey <46309555+Rabindra-pandey@users.noreply.github.com>
2020-02-01 14:23:47 +0530

    Update common.class.php

--- core/common.class.php
+++ core/common.class.php
@@ -28,6 +28,7 @@
 			echo '</pre>';
 		}
 	}
+	include_once(APPS_PATH."/application/config/session.php");
 
 	$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
